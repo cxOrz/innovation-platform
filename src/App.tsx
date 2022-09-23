61cc7bb0c71088192a7a95781ddc5218daa96c38
cxOrz <cxOrz@outlook.com>
2022-09-23 16:39:22 +0800

    升级react-router / vite；修改懒加载策略；

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ function App() {
         <Outlet />
       </div>
       <footer>
-        Copyright © 2022 miaochenxi | Apache License
+        Copyright © 2022 cxOrz | Apache License
       </footer>
       <Snackbar open={snackbar.open} autoHideDuration={3000} onClose={handleAlertClose}>
         <Alert onClose={handleAlertClose} severity={snackbar.severity} sx={{ width: '100%' }}>
